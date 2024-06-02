9e6480da9741766eb77673d48733b735b0945a51
Arkadiusz Biel - bielu <bielu@bielu.pl>
2024-06-02 11:47:19 +0100

    chore: POC translation for bool queries

--- src/Bielu.BetterSearch.Abstractions/Services/ISubQueryTranslator.cs
+++ src/Bielu.BetterSearch.Abstractions/Services/ISubQueryTranslator.cs
@@ -3,8 +3,13 @@ using FluentResults;
 
 namespace Bielu.BetterSearch.Abstractions.Services;
 
-public interface ISubQueryTranslator<T,TQuery> where T : class, ISearchSubQuery
+public interface ISubQueryTranslator<T,TQuery> : ISubQueryTranslator<TQuery> where T : class, ISearchSubQuery
+{
+
+}
+
+public interface ISubQueryTranslator<TQuery>
 {
     bool CanTranslate(ISearchSubQuery query);
-    Task<Result<TQuery>> TranslateAsync(ISearchSubQuery query);
+    Task<Result<TQuery>> TranslateAsync(ISearchSubQuery query, IEnumerable<ISubQueryTranslator<TQuery>> subQueryTranslators);
 }
---
+++ src/Bielu.BetterSearch.Lifti/Services/Queries/BooleanSubQueryTranslator.cs
@@ -0,0 +1,27 @@
+﻿using Bielu.BetterSearch.Abstractions.Models;
+using Bielu.BetterSearch.Abstractions.Query;
+using Bielu.BetterSearch.Abstractions.Query.SubQueries;
+using Bielu.BetterSearch.Abstractions.Services;
+using FluentResults;
+using Lifti.Querying;
+
+namespace Bielu.BetterSearch.Lifti.Services.Queries;
+
+public class BooleanSubQueryTranslator : ISubQueryTranslator<BoolSearchSubQuery,IQuery>
+{
+#pragma warning disable CA1822
+    public bool CanTranslate(ISearchSubQuery query) => query is BoolSearchSubQuery;
+#pragma warning restore CA1822
+
+    public Task<Result<IQuery>> TranslateAsync(ISearchSubQuery query, IEnumerable<ISubQueryTranslator<IQuery>> subQueryTranslators)
+    {
+        if (query is not BoolSearchSubQuery boolQuery)
+        {
+            return Task.FromResult(Result.Fail<IQuery>("Query is not a boolean query"));
+        }
+
+        var subQueries = boolQuery.NestedQueries.Select(subQuery => subQueryTranslators.First(translator => translator.CanTranslate(subQuery)).TranslateAsync(subQuery, subQueryTranslators)).ToList();
+
+
+    }
+}
